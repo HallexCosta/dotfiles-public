1eb0d6f4604762c7b85618b3e2de89d2e134a7f9
HallexCosta <hallex.costa@hotmail.com>
2023-08-19 19:03:17 -0300

    feat: add configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,61 +1,44 @@
-# ~/.config/fish/config.fish
-
-set fish_greeting ""
-
-set -gx TERM xterm-256color
-
-# theme
-set -g theme_color_scheme terminal-dark
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user yes
-set -g theme_hide_hostname no
-set -g theme_hostname always
-
-# aliases
-alias ls "ls -p -G"
-alias la "ls -A"
-alias ll "ls -l"
-alias lla "ll -A"
-alias g git
-command -qv nvim && alias vim nvim
-
-set -gx EDITOR nvim
-
-set -gx PATH bin $PATH
-set -gx PATH ~/bin $PATH
-set -gx PATH ~/.local/bin $PATH
-set -gx PATH $HOME/.cargo/bin $PATH
-
-# NodeJS
-set -gx PATH node_modules/.bin $PATH
-
-# Go
-set -g GOPATH $HOME/go
-set -gx PATH $GOPATH/bin $PATH
-
-# NVM
-function __check_rvm --on-variable PWD --description 'Do nvm stuff'
-  status --is-command-substitution; and return
-
-  if test -f .nvmrc; and test -r .nvmrc;
-    nvm use
-  else
-  end
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 end
 
-switch (uname)
-  case Darwin
-    source (dirname (status --current-filename))/config-osx.fish
-  case Linux
-    # Do nothing
-  case '*'
-    source (dirname (status --current-filename))/config-windows.fish
-end
-
-set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
-if test -f $LOCAL_CONFIG
-  source $LOCAL_CONFIG
-end
-
-# init theme starship
+# start theme
 starship init fish | source
+
+# add local bins in $PATH
+set PATH $HOME/.local/bin $HOME/.cargo/bin $PATH
+alias cat="bat --style=auto"
+alias ls="exa --icons"
+alias docker_clear="docker rmi (docker images -a -q) && docker rm (docker ps -a -q)"
+
+source /opt/asdf-vm/asdf.fish
+
+#set EXPO_PACKAGER_PROXY_URL 'http://192.168.0.8:19000'
+alias p=pnpm
+alias px='pnpm exec'
+
+fish_config theme choose "Rosé Pine Moon"
+
+# pnpm
+set -gx PNPM_HOME "/home/hallexcosta/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
+
+# load phpbrew to versionate php
+source ~/.phpbrew/shell/phpbrew.fish
+
+function dup
+  docker compose up -d $argv[1]
+end
+function dwn
+  docker compose down
+end
+function ddown
+  docker compose down
+end
+function dps
+  docker ps
+end
+function de
+  docker exec -it $argv[1] /bin/bash
+end
